1710180c11f6f1287873a10eab23dda119e0220f
Almar Heidarsson <almarheidars@gmail.com>
2017-03-25 02:34:32 -0400

    rid of nav link decoration. change all link states decoration to none. delete aside from css. delete css that wasnt being utilitized change color theme to cornflowerblue and lightblue. class to disclist ul, rid of list style. center aling collection title with class.

--- collection.php
+++ collection.php
@@ -5,20 +5,20 @@ include 'inc/header.php';
 ?>
 
 <div>
-    <h2>DiscCollection</h2>
+    <h2 class="collection-title">DiscCollection</h2>
         <ul>
             <?php 
             foreach (getDiscList() as $disc) {
             echo "<div>" . PHP_EOL . 
                         "<header>" . PHP_EOL .
                             "<h1>" . $disc['name'] . " - " . $disc['brand'] . "</h1>" . PHP_EOL .
-                            "<p>" . "<ul><li>" . $disc['type'] . "</li><li>" . $disc['skill'] . "</li><li>" . $disc['stability'] . "</li><li>" . $disc['plastic'] . "</li></ul>" . "</p>" . PHP_EOL .
+                            "<p>" . "<ul class='disclist'><li>" . $disc['type'] . "</li><li>" . $disc['skill'] . "</li><li>" . $disc['stability'] . "</li><li>" . $disc['plastic'] . "</li></ul>" . "</p>" . PHP_EOL .
                         "</header>" . PHP_EOL .
                         "<div>" . PHP_EOL .
-                            "<p>" . "<ul><li>" . "Speed: " . $disc['speed'] . "</li><li>" . "Glide: " . $disc['glide'] . "</li><li>" . "Turn: " .  $disc['turn'] . "</li><li>" . "Fade: " . $disc['fade'] . "</li></ul>" . "</p>" . PHP_EOL .
+                            "<p>" . "<ul class='disclist'><li>" . "Speed: " . $disc['speed'] . "</li><li>" . "Glide: " . $disc['glide'] . "</li><li>" . "Turn: " .  $disc['turn'] . "</li><li>" . "Fade: " . $disc['fade'] . "</li></ul>" . "</p>" . PHP_EOL .
                         "</div>" . PHP_EOL .
                         "<div>" . PHP_EOL .
-                            "<p>" . "<ul><li>" . "Weight: " . $disc['weight'] . "g" . "</li><li>" . $disc['info'] . "</li></ul>" . "</p>" . PHP_EOL .
+                            "<p>" . "<ul class='disclist'><li>" . "Weight: " . $disc['weight'] . "g" . "</li><li>" . $disc['info'] . "</li></ul>" . "</p>" . PHP_EOL .
                         "</div>" . PHP_EOL .
                 "</div>" . PHP_EOL;
             }
